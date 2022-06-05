52f3c588ff02fba401d3b332601fee441c158c55
Mike Smullin <mike@mikesmullin.com>
2022-06-05 13:24:57 -0600

    add notes

--- make.bat
+++ make.bat
@@ -13,6 +13,9 @@ echo clean...
 del main.obj main.exe
 
 echo compile and link...
+REM see https://docs.microsoft.com/en-us/cpp/build/reference/compiler-command-line-syntax?view=msvc-170
+REM see https://docs.microsoft.com/en-us/cpp/build/reference/i-additional-include-directories?view=msvc-170
+REM see https://docs.microsoft.com/en-us/cpp/build/reference/libpath-additional-libpath?view=msvc-170
 cl ../main.cpp /std:c++17 /MD /I ../vendor/glfw/include /link user32.lib shell32.lib gdi32.lib opengl32.lib /LIBPATH:../vendor/glfw/lib-vc2019 glfw3.lib 
 cd ..
 pause
