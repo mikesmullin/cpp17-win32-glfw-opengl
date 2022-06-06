cd0f89df200e403fa15ec2168dbbedfce7940179
Mike Smullin <mike@mikesmullin.com>
2022-06-05 22:11:06 -0600

    dry

--- src/IndexBuffer.cpp
+++ src/IndexBuffer.cpp
@@ -5,7 +5,7 @@ IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count)
   : m_Count(count)
 {
   GLCall(glGenBuffers(1, &m_RendererId));
-  GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererId));
+  IndexBuffer::Bind();
   GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
 }
 
