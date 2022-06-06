81414d42dc54c2daa16c03c5889725834a8d7f3e
Mike Smullin <mike@mikesmullin.com>
2022-06-05 19:10:49 -0600

    if we needed to select shader, array buffer, vertex attribs, index buffer ... for every draw call (ie. drawing multiple objects to screen)

--- main.cpp
+++ main.cpp
@@ -98,9 +98,9 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 
 static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
-  GLCall(unsigned int program = glCreateProgram());
-  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-  unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+  // GLCall(unsigned int program = glCreateProgram());
+  // unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  // unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
   GLCall(glAttachShader(program, vs));
   GLCall(glAttachShader(program, fs));
@@ -157,20 +157,22 @@ int main(void)
     2, 3, 0
   };
 
+  // unsigned int vao;
+  // GLCall(glGenVertexArrays(1, &vao));
+  // GLCall(glBindVertexArray(vao));
+
   unsigned int buffer;
   GLCall(glGenBuffers(1, &buffer));
   GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
   GLCall(glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW));
 
-  GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
   GLCall(glEnableVertexAttribArray(0));
+  GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
   unsigned int ibo;
   GLCall(glGenBuffers(1, &ibo));
   GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
   GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
-
-
 
   ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
   unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -178,6 +180,10 @@ int main(void)
   GLCall(int location = glGetUniformLocation(shader, "u_Color"));
   ASSERT(location != -1);
   GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+  GLCall(glUseProgram(0));
+  GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+  GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
   float r = 0.0f;
   float increment = 0.05f;
@@ -188,7 +194,15 @@ int main(void)
     GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
     // draw
+    GLCall(glUseProgram(shader));
     GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
     GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
     if (r > 1.0f)
